f800f385b1e33b9cf87cafafa2dde47be8a3fc6e
efrengamez <abinadigamezrodriguez@gmail.com>
2026-02-08 20:33:34 -0700

    lol

--- denisse/script.js
+++ denisse/script.js
@@ -3,6 +3,9 @@ let intentos = 0;
 function si() {
   document.getElementById("pregunta").style.display = "none";
   document.getElementById("invitacion").style.display = "block";
+
+  const musica = document.getElementById("musica");
+  musica.play();
 }
 
 function no() {
@@ -10,13 +13,11 @@ function no() {
 
   const mensajes = [
     "mmm… ¿segura? 👀",
-    "piénsalo bien 😌",
-    "un elotito no se rechaza",
+    "un elotito no se rechaza 🌽",
+    "ándale di que sí 😌",
     "ok pero… be my valentin? 💗"
   ];
 
-  document.getElementById("texto").innerHTML =
-    mensajes[intentos % mensajes.length];
+  alert(mensajes[intentos % mensajes.length]);
 }
-
 

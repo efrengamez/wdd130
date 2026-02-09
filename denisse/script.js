a9601c689284c04d4b99ad9ad72ee7774057e46e
efrengamez <abinadigamezrodriguez@gmail.com>
2026-02-08 20:13:05 -0700

    lol

--- denisse/script.js
+++ denisse/script.js
@@ -2,20 +2,21 @@ let intentos = 0;
 
 function si() {
   document.getElementById("texto").innerHTML =
-    "sabía que dirías que sí 🤍<br>ya somos valentineeee ✨";
+    "JAJA sabía que sí 🤍<br>entonces ya somos oficialmente 💘";
 }
 
 function no() {
   intentos++;
 
-  const texto = document.getElementById("texto");
-
   const mensajes = [
-    "¿segura? 🥺",
-    "piénsalo otra vez, sí? 👀",
-    "mmm… creo que te equivocaste 😌",
-    "ok, pero… ¿quieres ser mi valentineeee? 🤍"
+    "mmm… ¿segura? 👀",
+    "piénsalo con el corazón 😌",
+    "valentín no aceptaría un no",
+    "ok pero… be my valentin? 💗"
   ];
 
-  texto.innerHTML = mensajes[intentos % mensajes.length];
+  document.getElementById("texto").innerHTML =
+    mensajes[intentos % mensajes.length];
 }
+
+
